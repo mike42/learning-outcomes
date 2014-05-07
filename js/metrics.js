e04e245f909f380f360b17c6098d3a666f6812a9
Michael Billington <michael.billington@gmail.com>
2014-05-07 21:16:15 +1000

    add parameters, eclipse project

--- js/metrics.js
+++ js/metrics.js
@@ -1,51 +1,63 @@
-function read_sentence(text) {
-	/* Chop up the source into sentences, each with a list of words */
+/**
+ * Chop up the source into sentences and words for computing metrics.
+ * 
+ * @param text Text to read in
+ * @returns Array of sentences contained in the text, with each sentence being an array of words. All words will be returned in lowercase.
+ */
+function metric_read_passage(text) {
 	var prevIsAlph = false; // Previous character was part of a word
 	var isAlph, isEndSentence, c;
 	var word = '';
 	var alph = 'abcdefghijklmnopqrstuvwxyz-\'';
 	var endSentence = '\n.?!';
-	
+
 	var sentence = [];
 	var sentences = [];
-	
-	for(i = 0; i < text.length; i++) {
-		c = text.substring(i, i+1).toLowerCase();
+
+	for (i = 0; i < text.length; i++) {
+		c = text.substring(i, i + 1).toLowerCase();
 		isAlph = (alph.indexOf(c) != -1);
 		isEndSentence = (endSentence.indexOf(c) != -1);
-		if(isAlph == false && prevIsAlph == true) {
-			sentence.push(word);
+		if (isAlph == false && prevIsAlph == true) {
+			sentence.push([word]);
 			word = '';
-		} else if(isAlph == true) {
+		} else if (isAlph == true) {
 			word += c;
-		} else if(sentence.length > 0 && isEndSentence) {
+		} else if (sentence.length > 0 && isEndSentence) {
 			sentences.push(sentence);
 			sentence = [];
 		}
 		prevIsAlph = isAlph;
 	}
-	if(word.length > 0) {
-		sentence.push(word);
+	if (word.length > 0) {
+		sentence.push({word});
 		word = '';
 	}
-	if(sentence.length > 0) {
-		sentences.push(word);
-		
+	if (sentence.length > 0) {
+		sentences.push(sentence);
+
 	}
-	
-	console.log(sentences);
+	return sentences;
 }
 
-function metric_readability(text) {
+function metric_passage_readability(sentences) {
 	/* Calculate the readability score */
+	
 }
 
-function metric_keywords(text) {
-	/* Count keywords */
+function metric_word_syllable(word) {
+	/* Count the number of 
+}
 
+function metric_passage_wordcount(sentences) {
+	
+}
+
+function metric_passage_keywords(passage) {
+	
 }
 
 function test(text) {
-	read_sentence(text);
-	//console.log(metric_wordcount(text));
+	var passage = metric_read_passage(text);
+	console.log(passage);
 }
